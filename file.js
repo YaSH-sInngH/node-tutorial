eb58dd5540eda9ebc21ab5e66f136198f4c79aa9
Yash <singhyash0026@gmail.com>
2024-10-15 14:19:58 +0530

    fs-handle

--- file.js
+++ file.js
@@ -1,11 +1,11 @@
 const fs = require('fs'); 
 //Sync
-// fs.writeFileSync("./test.txt", "Hi, this is the file created by writeFileSync method from file handling or fs operator present in node.");
+ fs.writeFileSync("./test.txt", "Hi, this is the file created by writeFileSync method from file handling or fs operator present in node.");
 
 // In this we are trying to read file synchronously so that the method 'readFileSync'
 // can return the values in the result variable.
-// const result = fs.readFileSync("./contact.txt", "utf-8");
-// console.log(result);
+ const result = fs.readFileSync("./contact.txt", "utf-8");
+ console.log(result);
 
 // But in asynchronous the nothing will be returned, as it try to take callback.
 
